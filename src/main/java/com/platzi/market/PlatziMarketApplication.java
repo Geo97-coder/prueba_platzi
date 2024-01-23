c394cdc413b2974254325e26c9f86cba26669267
Geo97-coder <genadetapia@gmail.com>
2024-01-23 18:21:45 -0300

    Creacion de Datasource

--- src/main/java/com/platzi/market/PlatziMarketApplication.java
+++ src/main/java/com/platzi/market/PlatziMarketApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PlatziMarketApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(PlatziMarketApplication.class, args);
 	}
-
 }
+
